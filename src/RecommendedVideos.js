9df88a885ce2c9def25438e3dfc2f62cb5b9bdf6
gkaragoz <gkaragoz96@gmail.com>
2020-08-03 17:54:01 +0300

    Create ChannelRow and VideoRow components and finalize the project.

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -17,36 +17,36 @@ function RecommendedVideos() {
           image="https://i.ytimg.com/an_webp/9KioWmxSDWc/mqdefault_6s_480x270.webp?du=3000&sqp=CMb1n_kF&rs=AOn4CLC2927MggM4UmxlIoUDsruHavt_Bw"
         />
         <VideoCard
-          title="Melek Mosso - Vursalar Ölemem (cover)"
-          views="88M views"
-          timestamp="2 years ago"
-          channelImage="https://yt3.ggpht.com/a/AATXAJxp8FBC49G8iqzDKFFm-A0pz9-LZjhfJihrGSr1KA=s88-c-k-c0x00ffffff-no-rj"
-          channel="melek mosso"
-          image="https://i.ytimg.com/an_webp/9KioWmxSDWc/mqdefault_6s_480x270.webp?du=3000&sqp=CMb1n_kF&rs=AOn4CLC2927MggM4UmxlIoUDsruHavt_Bw"
+          title="Arkadaşım Hoşgeldin - 4. Bölüm"
+          views="1.2M views"
+          timestamp="3 years ago"
+          channelImage="https://yt3.ggpht.com/a-/AOh14Gh9qwPGdd6RoeKuQ8oTECk-rxsaPJHMckMCZQ=s68-c-k-c0x00ffffff-no-rj-mo"
+          channel="Beşiktaş Kültür Merkezi (BKM)"
+          image="https://i.ytimg.com/an_webp/V9duBs2M1kk/mqdefault_6s_480x270.webp?du=3000&sqp=CL2ooPkF&rs=AOn4CLB1Oh5G8Pra-VpH2UPuj8V_labtKA"
         />
         <VideoCard
-          title="Melek Mosso - Vursalar Ölemem (cover)"
-          views="88M views"
-          timestamp="2 years ago"
-          channelImage="https://yt3.ggpht.com/a/AATXAJxp8FBC49G8iqzDKFFm-A0pz9-LZjhfJihrGSr1KA=s88-c-k-c0x00ffffff-no-rj"
-          channel="melek mosso"
-          image="https://i.ytimg.com/an_webp/9KioWmxSDWc/mqdefault_6s_480x270.webp?du=3000&sqp=CMb1n_kF&rs=AOn4CLC2927MggM4UmxlIoUDsruHavt_Bw"
+          title="Melis Fis - Dur Diyemem (Official Video)"
+          views="4.2M views"
+          timestamp="7 months ago"
+          channelImage="https://yt3.ggpht.com/a-/AOh14Ggyvmd_K--keoa3U06UgEIJlffE-IRj-k3Mnw=s68-c-k-c0x00ffffff-no-rj-mo"
+          channel="Melis Fis"
+          image="https://i.ytimg.com/an_webp/LDvB8hezaXk/mqdefault_6s_480x270.webp?du=3000&sqp=CPmsoPkF&rs=AOn4CLAwePK8AGNw002j4XUe5gHBn6bpmg"
         />
         <VideoCard
-          title="Melek Mosso - Vursalar Ölemem (cover)"
-          views="88M views"
-          timestamp="2 years ago"
-          channelImage="https://yt3.ggpht.com/a/AATXAJxp8FBC49G8iqzDKFFm-A0pz9-LZjhfJihrGSr1KA=s88-c-k-c0x00ffffff-no-rj"
-          channel="melek mosso"
-          image="https://i.ytimg.com/an_webp/9KioWmxSDWc/mqdefault_6s_480x270.webp?du=3000&sqp=CMb1n_kF&rs=AOn4CLC2927MggM4UmxlIoUDsruHavt_Bw"
+          title="If Programming Was An Anime"
+          views="3.7M views"
+          timestamp="5 months ago"
+          channelImage="https://yt3.ggpht.com/a-/AOh14Git2v-jQceuRB3F_T8Fnfu2BvlP9p_9sTnWpA=s68-c-k-c0x00ffffff-no-rj-mo"
+          channel="Joma Tech"
+          image="https://i.ytimg.com/an_webp/pKO9UjSeLew/mqdefault_6s_480x270.webp?du=3000&sqp=CMyooPkF&rs=AOn4CLCUNpWef19MtQ_tG4KZXCMbiXf0WQ"
         />
         <VideoCard
-          title="Melek Mosso - Vursalar Ölemem (cover)"
-          views="88M views"
-          timestamp="2 years ago"
-          channelImage="https://yt3.ggpht.com/a/AATXAJxp8FBC49G8iqzDKFFm-A0pz9-LZjhfJihrGSr1KA=s88-c-k-c0x00ffffff-no-rj"
-          channel="melek mosso"
-          image="https://i.ytimg.com/an_webp/9KioWmxSDWc/mqdefault_6s_480x270.webp?du=3000&sqp=CMb1n_kF&rs=AOn4CLC2927MggM4UmxlIoUDsruHavt_Bw"
+          title="Didomido - Saydım (Cover)"
+          views="396K views"
+          timestamp="1 years ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJyufULd7e_9Q8quACmbOr4Qdf9PZTjYszvZDL-R=s176-c-k-c0x00ffffff-no-rj-mo"
+          channel="Didem"
+          image="https://i.ytimg.com/an_webp/I2oXXHeX6pU/mqdefault_6s.webp?du=3000&sqp=CPGvoPkF&rs=AOn4CLDTeIqauxH2qB3V9sNI_ry6W0V4Uw"
         />
       </div>
     </div>
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,5 +1,7 @@
 import React from "react";
 import TuneOutlinedIcon from "@material-ui/icons/TuneOutlined";
+import ChannelRow from "./ChannelRow";
+import VideoRow from "./VideoRow";
 import "./SearchPage.css";
 
 function SearchPage() {
@@ -10,6 +12,57 @@ function SearchPage() {
         <h2>FILTER</h2>
       </div>
       <hr />
+
+      <ChannelRow
+        image="https://yt3.ggpht.com/a/AATXAJxp8FBC49G8iqzDKFFm-A0pz9-LZjhfJihrGSr1KA=s176-c-k-c0x00ffffff-no-rj-mo"
+        channel="melek mosso"
+        verified
+        subs="436K"
+        noOfVideos={20}
+        description="Vokal, besteci, müzisyen"
+      />
+
+      <hr />
+
+      <VideoRow
+        views="38K"
+        subs="436K"
+        description="Mezunu olduğum bölüm ve flüt hocam Zeynep GERGİN'le birlikte bir şeyler yapabilmiş olmaktan çok mutlu oldum. Üstelik ilk defa .."
+        timestamp="3 weeks ago"
+        channel="melek mosso"
+        title="Melek Mosso ft. ADÜ Güzel Sanatlar - Ederlezi (Goran Bregovic)"
+        image="https://i.ytimg.com/an_webp/j2hFHO2Rl5M/mqdefault_6s.webp?du=3000&sqp=CMq2oPkF&rs=AOn4CLDrI9vWV5HSZ-hhEqU3izbd4I75og"
+      />
+
+      <VideoRow
+        views="1M"
+        subs="436K"
+        description="Arzular Arsız'ı ilk söylediğimde kafamda olan üçlemenin son şarkısıydı ''Yar'' O kadar çok söylüyordum ki ; hem kendi kendime ..."
+        timestamp="1 month ago"
+        channel="melek mosso"
+        title="Melek Mosso - Yar (Yıldız Tilbe Cover)"
+        image="https://i.ytimg.com/an_webp/lxqI3CHqPsE/mqdefault_6s.webp?du=3000&sqp=CKqPoPkF&rs=AOn4CLDFncV1Ymi_26JWPjvjaVCO4dSlKQ"
+      />
+
+      <VideoRow
+        views="31M"
+        subs="436K"
+        description="Melek Mosso - Arzular Arsız (Yıldız Tilbe Cover) Ft. Veys Colak Kamera: Fırat Özmüş Video Edit: Veys Colak Mix: Can Ozan ..."
+        timestamp="2 years ago"
+        channel="melek mosso"
+        title="Melek Mosso - Arzular Arsız"
+        image="https://i.ytimg.com/an_webp/vwawFAgVg5M/mqdefault_6s.webp?du=3000&sqp=CIWioPkF&rs=AOn4CLDAUlbtf7Epl0qHYBrXNVRziTSVXA"
+      />
+
+      <VideoRow
+        views="4.1M"
+        subs="436K"
+        description="Uzun zamandır sizlere dinletmek istediğim şarkım ''Gönül gözü'' sonunda renge ve şekle büründü. Söylediğim her sözün kalbinize konması dileğimle. "
+        timestamp="9 months ago"
+        channel="melek mosso"
+        title="Melek Mosso - Gönül gözü"
+        image="https://i.ytimg.com/an_webp/11I7wrHJDaY/mqdefault_6s.webp?du=3000&sqp=CMq8oPkF&rs=AOn4CLDa65bv8smhCIB1KaAeYl1uKwylFg"
+      />
     </div>
   );
 }
