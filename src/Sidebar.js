dce7db2027a3f3820c657440467799dc93245957
gkaragoz <gkaragoz96@gmail.com>
2020-08-03 15:43:33 +0300

    Finalize sidebar styling and jsx stuff.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,17 +1,30 @@
 import React from "react";
-import "./Sidebar.css";
 import HomeIcon from "@material-ui/icons/Home";
 import WhatshotIcon from "@material-ui/icons/Whatshot";
 import SubscriptionIcon from "@material-ui/icons/Subscriptions";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import HistoryIcon from "@material-ui/icons/History";
+import OndemandVideoIcon from "@material-ui/icons/OndemandVideo";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined";
+import ExpandMoreOutlinedIcon from "@material-ui/icons/ExpandMoreOutlined";
 
 import SidebarRow from "./SidebarRow";
+import "./Sidebar.css";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <SidebarRow Icon={HomeIcon} title="Home" />
+      <SidebarRow selected Icon={HomeIcon} title="Home" />
       <SidebarRow Icon={WhatshotIcon} title="Trending" />
       <SidebarRow Icon={SubscriptionIcon} title="Subscription" />
+      <hr />
+      <SidebarRow Icon={VideoLibraryIcon} title="Library" />
+      <SidebarRow Icon={HistoryIcon} title="History" />
+      <SidebarRow Icon={OndemandVideoIcon} title="Your videos" />
+      <SidebarRow Icon={WatchLaterIcon} title="Watch Later" />
+      <SidebarRow Icon={ThumbUpAltOutlinedIcon} title="Liked videos" />
+      <SidebarRow Icon={ExpandMoreOutlinedIcon} title="Show more" />
     </div>
   );
 }
--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./SidebarRow.css";
 
-function SidebarRow({ Icon, title }) {
+function SidebarRow({ selected, Icon, title }) {
   return (
-    <div className="sidebarRow">
-      <Icon />
-      <h2>{title}</h2>
+    <div className={`sidebarRow ${selected && "selected"}`}>
+      <Icon className={"sidebarRow_icon"} />
+      <h2 className="sidebarRow_title">{title}</h2>
     </div>
   );
 }
