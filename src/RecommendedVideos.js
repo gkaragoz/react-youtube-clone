1330644f511f0ad62d16dd3213f049cab2e1deac
gkaragoz <gkaragoz96@gmail.com>
2020-08-03 16:34:16 +0300

    Create VideoCard component and implement it

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,10 +1,54 @@
 import React from "react";
+import VideoCard from "./VideoCard";
+
 import "./RecommendedVideos.css";
 
 function RecommendedVideos() {
   return (
     <div className="recommendedVideos">
-      <h2>This is where videos go</h2>
+      <h2>Recommended</h2>
+      <div className="recommendedVideos_videos">
+        <VideoCard
+          title="Melek Mosso - Vursalar Ölemem (cover)"
+          views="88M views"
+          timestamp="2 years ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJxp8FBC49G8iqzDKFFm-A0pz9-LZjhfJihrGSr1KA=s88-c-k-c0x00ffffff-no-rj"
+          channel="melek mosso"
+          image="https://i.ytimg.com/an_webp/9KioWmxSDWc/mqdefault_6s_480x270.webp?du=3000&sqp=CMb1n_kF&rs=AOn4CLC2927MggM4UmxlIoUDsruHavt_Bw"
+        />
+        <VideoCard
+          title="Melek Mosso - Vursalar Ölemem (cover)"
+          views="88M views"
+          timestamp="2 years ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJxp8FBC49G8iqzDKFFm-A0pz9-LZjhfJihrGSr1KA=s88-c-k-c0x00ffffff-no-rj"
+          channel="melek mosso"
+          image="https://i.ytimg.com/an_webp/9KioWmxSDWc/mqdefault_6s_480x270.webp?du=3000&sqp=CMb1n_kF&rs=AOn4CLC2927MggM4UmxlIoUDsruHavt_Bw"
+        />
+        <VideoCard
+          title="Melek Mosso - Vursalar Ölemem (cover)"
+          views="88M views"
+          timestamp="2 years ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJxp8FBC49G8iqzDKFFm-A0pz9-LZjhfJihrGSr1KA=s88-c-k-c0x00ffffff-no-rj"
+          channel="melek mosso"
+          image="https://i.ytimg.com/an_webp/9KioWmxSDWc/mqdefault_6s_480x270.webp?du=3000&sqp=CMb1n_kF&rs=AOn4CLC2927MggM4UmxlIoUDsruHavt_Bw"
+        />
+        <VideoCard
+          title="Melek Mosso - Vursalar Ölemem (cover)"
+          views="88M views"
+          timestamp="2 years ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJxp8FBC49G8iqzDKFFm-A0pz9-LZjhfJihrGSr1KA=s88-c-k-c0x00ffffff-no-rj"
+          channel="melek mosso"
+          image="https://i.ytimg.com/an_webp/9KioWmxSDWc/mqdefault_6s_480x270.webp?du=3000&sqp=CMb1n_kF&rs=AOn4CLC2927MggM4UmxlIoUDsruHavt_Bw"
+        />
+        <VideoCard
+          title="Melek Mosso - Vursalar Ölemem (cover)"
+          views="88M views"
+          timestamp="2 years ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJxp8FBC49G8iqzDKFFm-A0pz9-LZjhfJihrGSr1KA=s88-c-k-c0x00ffffff-no-rj"
+          channel="melek mosso"
+          image="https://i.ytimg.com/an_webp/9KioWmxSDWc/mqdefault_6s_480x270.webp?du=3000&sqp=CMb1n_kF&rs=AOn4CLC2927MggM4UmxlIoUDsruHavt_Bw"
+        />
+      </div>
     </div>
   );
 }
---
+++ src/VideoCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+
+import "./VideoCard.css";
+
+function VideoCard({ image, title, channel, views, timestamp, channelImage }) {
+  return (
+    <div className="videoCard">
+      <img className="videoCard_thumbnail" src={image} alt="" />
+      <div className="videoCard_info">
+        <Avatar className="videoCard_avatar" alt={channel} src={channelImage} />
+        <div className="videoCard_text">
+          <h4>{title}</h4>
+          <p>{channel}</p>
+          <p>
+            {views} • {timestamp}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default VideoCard;
